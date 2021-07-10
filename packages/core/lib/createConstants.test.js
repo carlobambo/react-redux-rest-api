8fe2bc187c425d79593c7b7bc3c529036cb41319
John Carlo Bambo <carlobambo@gmail.com>
2021-07-10 17:44:51 +0800

    Create CRUD async constants

--- packages/core/lib/createConstants.test.js
+++ packages/core/lib/createConstants.test.js
@@ -1,5 +1,7 @@
 import {
   createAsyncActionConst,
+  create,
+  createCRUDConst,
 } from "./createConstants";
 
 test('create async constants for LIST', () => {
@@ -8,6 +10,37 @@ test('create async constants for LIST', () => {
     START: 'LIST_START',
     SUCCESS: 'LIST_SUCCESS',
     FAILED: 'LIST_FAILED',
+  });
+});
+test('create crud constants for USERS', () => {
+  const moduleName = 'USERS';
+  const crudConsts = createCRUDConst(moduleName);
+  expect(crudConsts).toEqual({
+    CREATE: {
+      START: 'USERS_CREATE_START',
+      SUCCESS: 'USERS_CREATE_SUCCESS',
+      FAILED: 'USERS_CREATE_FAILED',
+    },
+    UPDATE: {
+      START: 'USERS_UPDATE_START',
+      SUCCESS: 'USERS_UPDATE_SUCCESS',
+      FAILED: 'USERS_UPDATE_FAILED',
+    },
+    PATCH: {
+      START: 'USERS_PATCH_START',
+      SUCCESS: 'USERS_PATCH_SUCCESS',
+      FAILED: 'USERS_PATCH_FAILED',
+    },
+    DELETE: {
+      START: 'USERS_DELETE_START',
+      SUCCESS: 'USERS_DELETE_SUCCESS',
+      FAILED: 'USERS_DELETE_FAILED',
+    },
+    LIST: {
+      START: 'USERS_LIST_START',
+      SUCCESS: 'USERS_LIST_SUCCESS',
+      FAILED: 'USERS_LIST_FAILED',
+    },
   });
 });
 
--- packages/core/lib/createConstants.ts
+++ packages/core/lib/createConstants.ts
@@ -3,12 +3,33 @@ const SUCCESS = "SUCCESS";
 const FAILED = "FAILED";
 const SEPARATOR = '_';
 
+const CREATE = 'CREATE';
+const UPDATE = 'UPDATE';
+const PATCH = 'PATCH';
+const LIST = 'LIST';
+const DELETE = 'DELETE';
+
+const CRUD_OPERATIONS = [
+  CREATE,
+  UPDATE,
+  PATCH,
+  LIST,
+  DELETE,
+];
+
 const join = (pref, suff) => `${pref}${SEPARATOR}${suff}`;
 
-type AsyncConsts = {
+export interface AsyncConsts {
   START: string,
   SUCCESS: string,
   FAILED: string,
+};
+export type CRUDConsts = {
+  CREATE: AsyncConsts,
+  UPDATE: AsyncConsts,
+  PATCH: AsyncConsts,
+  DELETE: AsyncConsts,
+  LIST: AsyncConsts,
 };
 
 const createAsyncActionConst = (prefix : string) : AsyncConsts => {
@@ -17,7 +38,18 @@ const createAsyncActionConst = (prefix : string) : AsyncConsts => {
     SUCCESS: join(prefix, SUCCESS),
     FAILED: join(prefix, FAILED),
   };
-}
+};
+
+const createCRUDConst = (moduleName : string) : CRUDConsts => {
+  const crudConsts = CRUD_OPERATIONS.reduce((allConsts : {}, op : string) => {
+    const opPrefix = join(moduleName, op);
+    return {
+      ...allConsts,
+      [op]: createAsyncActionConst(opPrefix),
+    };
+  }, {}) as CRUDConsts;
+  return crudConsts;
+};
 
 export {
   START,
@@ -25,4 +57,5 @@ export {
   FAILED,
   SEPARATOR,
   createAsyncActionConst,
+  createCRUDConst,
 };
